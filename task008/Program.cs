a01b08a59855ffd03966680d8d5fa934c0588137
Akkenton <akkenton.msk@gmail.com>
2022-11-24 00:07:52 +0300

    Решение задачи №8

--- task008/Program.cs
+++ task008/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+//---------  START -----------
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int count = 0; // переменная в цикле, в которую будет записывать шаг +/- 2
+if (num == 1)
+{
+    Console.WriteLine("Так как число является 1, то целого четного числа от 1 до 1 по условиям задачи не существует!");
+}
+else
+{
+    if (num > 1)
+    {
+        count = count + 2;
+        while (count <= num)
+        {
+            Console.Write(count + " ");
+            count = count + 2;
+        }
+    }
+    if (num <= 0) //ноль в математике четное число
+    {
+        while (count >= num)
+        {
+            Console.Write(count + " ");
+            count = count - 2;
+        }
+    }
+}
+
+
+//---------  END -------------
